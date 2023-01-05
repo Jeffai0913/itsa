3bea00cfcb733053d7c2be16342ef85b5e3cfd18
Jeffai0913 <90823162+Jeffai0913@users.noreply.github.com>
2023-01-05 14:33:31 +0800

    Add files via upload

---
+++ find_extreme.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    float input[10];
+    for(int i=0; i<10; i++){
+        scanf("%f", &input[i]);
+    }
+    //max
+    float max=input[0];
+    for(int i=0; i<10; i++){
+        if(input[i] > max) max=input[i];
+    }
+    //min
+    float min=input[0];
+    for(int i=0; i<10; i++){
+        if(input[i] < min) min=input[i];
+    }
+    printf("maximum:%.2f\n", max);
+    printf("minimum:%.2f\n", min);
+}
